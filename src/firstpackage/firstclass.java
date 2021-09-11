e0eecb178969ba76617fe28b63a2977e04a272c9
ferhatm@gmail.com <ferhatmandz@gmail>
2021-09-11 03:35:25 -0400

    second commit from local master

--- src/firstpackage/firstclass.java
+++ src/firstpackage/firstclass.java
@@ -1,5 +1,10 @@
 package firstpackage;
 
 public class firstclass {
+	public static void main(String[] args) {
+		System.out.println("first commit from local master branch");
+		System.out.println("second commit from local master");
+		
+	}
 
 }
